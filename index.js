e4bcd0ea3e6fad5212c702a62bf51ef66fc52d9e
saikat <tawhidulislam3482@gmail.com>
2023-11-26 22:01:55 +0600

    today final push

--- index.js
+++ index.js
@@ -33,12 +33,50 @@ async function run() {
     const contestCollection = client
       .db("last-assignment")
       .collection("contests");
+    const userCollection = client.db("last-assignment").collection("users");
 
+    // user api
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const exitingUser = await userCollection.findOne(query);
+      if (exitingUser) {
+        return res.send({ message: "user already exit", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get('/users',async(req,res)=>{
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
+    app.delete('/users/:id',async(req,res)=>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
+    })
+    app.patch('/users/admin/:id',async(req,res)=>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const updatedDoc = {
+        $set:{
+          role:'admin'
+        }
+      }
+      const result = await userCollection.updateOne(query,updatedDoc)
+      res.send(result)
+
+    })
+
+    // contests api
     app.get("/contests", async (req, res) => {
       const result = await contestCollection.find().toArray();
       res.send(result);
     });
 
+    // registers api
     app.get("/registers", async (req, res) => {
       const email = req.query.email;
       const query = { email: email };
@@ -51,12 +89,12 @@ async function run() {
       const result = await resgisterCollection.insertOne(resUser);
       res.send(result);
     });
-    app.delete('/registers/:id',async(req,res)=>{
-      const id = req.params.id
-      const query = {_id: new ObjectId(id)} 
-      const result = await resgisterCollection.deleteOne(query)
-      res.send(result)
-    })
+    app.delete("/registers/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await resgisterCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
