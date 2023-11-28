a0ba4e26f78ba055219628c3c144169cdb9437b1
saikat <tawhidulislam3482@gmail.com>
2023-11-28 13:17:53 +0600

    work hard

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 //Middleware
@@ -34,11 +36,72 @@ async function run() {
       .db("last-assignment")
       .collection("contests");
     const userCollection = client.db("last-assignment").collection("users");
+    const paymentCollection = client.db("last-assignment").collection("payments");
+    const winnerCollection = client.db("last-assignment").collection("winners");
+    const resMemberCollection = client.db("last-assignment").collection("resMembers");
+    const contestsCountCollection = client.db("last-assignment").collection("contestCounts");
+
+
+    // jwt api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    //middleware jwt
+    const verifyToken = (req, res, next) => {
+      // console.log("insite verify", req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized accesss" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if (error) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+        req.decoded = decoded;
+
+        next();
+      });
+    };
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
+    //
+    // resMembers api
+    app.get('/resMembers', async (req, res) => {
+      const email = req.query.email; 
+      const filter = { email: email };
+      const result = await resMemberCollection.find(filter).toArray();
+      res.send(result);
+    });
+    
+    app.post('/resMembers',async(req,res)=>{
+      const user = req.body;
+      const query = { email: user?.email };
+      const exitingres = await resMemberCollection.findOne(query);
+      if (exitingres) {
+        return res.send({ message: "Registration already complete", insertedId: null });
+      }
+      const result = await resMemberCollection.insertOne(user);
+      res.send(result);
+    })
 
     // user api
     app.post("/users", async (req, res) => {
       const user = req.body;
-      const query = { email: user.email };
+      const query = { email: user?.email };
       const exitingUser = await userCollection.findOne(query);
       if (exitingUser) {
         return res.send({ message: "user already exit", insertedId: null });
@@ -47,32 +110,88 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/users',async(req,res)=>{
-      const result = await userCollection.find().toArray()
-      res.send(result)
-    })
-    app.delete('/users/:id',async(req,res)=>{
-      const id = req.params.id
-      const query = {_id: new ObjectId(id)}
-      const result = await userCollection.deleteOne(query)
-      res.send(result)
-    })
-    app.patch('/users/admin/:id',async(req,res)=>{
-      const id = req.params.id
-      const query = {_id: new ObjectId(id)}
-      const updatedDoc = {
-        $set:{
-          role:'admin'
-        }
+    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/users/admin/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" });
       }
-      const result = await userCollection.updateOne(query,updatedDoc)
-      res.send(result)
 
-    })
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      res.send({ admin });
+    });
 
+    app.delete("/users/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+    app.patch(
+      "/users/admin/:id",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const updatedDoc = {
+          $set: {
+            role: "admin",
+          },
+        };
+        const result = await userCollection.updateOne(query, updatedDoc);
+        res.send(result);
+      }
+    );
     // contests api
     app.get("/contests", async (req, res) => {
       const result = await contestCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/contests/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await contestCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/contests", verifyToken, verifyAdmin, async (req, res) => {
+      const contest = req.body;
+      const result = await contestCollection.insertOne(contest);
+      res.send(result);
+    });
+    app.patch("/contests/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const item = req.body;
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          name: item.name,
+          count: item.count,
+          fee: item.fee,
+          deadline: item.deadline,
+          contestDescription: item.contestDescription,
+          shortDescription: item.shortDescription,
+          contestPrize: item.contestPrize,
+          img: item.img,
+        },
+      };
+      const result = await contestCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    });
+    app.delete("/contests/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await contestCollection.deleteOne(query);
       res.send(result);
     });
 
@@ -95,6 +214,89 @@ async function run() {
       const result = await resgisterCollection.deleteOne(query);
       res.send(result);
     });
+
+    // payments api
+    app.post("/create-payment-intent", async (req, res) => {
+      const { fee } = req.body;
+      const amount = parseInt(fee * 100);
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+    app.get("/payments/:email", verifyToken, async (req, res) => {
+      const query = { email: req.params.email };
+      if (req.params.email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/payments", async (req, res) => {
+      const payment = req.body;
+      const query = {
+        _id: {
+          $in: payment.cartIds.map((id) => new ObjectId(id)),
+        },
+      };
+      console.log(query)
+      const deleteResult = await resgisterCollection.deleteMany(query);
+
+      const paymentResult = await paymentCollection.insertOne(payment);
+      res.send({ paymentResult, deleteResult });
+    });
+
+    // stats 
+    app.get("/admin-status",verifyToken,verifyAdmin, async (req, res) => {
+      const user = await userCollection.estimatedDocumentCount();
+      const menuItems = await contestCollection.estimatedDocumentCount();
+      const orderItems = await paymentCollection.estimatedDocumentCount();
+      const result = await paymentCollection
+        .aggregate([
+          {
+            $group: {
+              _id: null,
+              totalRevenue: {
+                $sum: "$fee",
+              },
+            },
+          },
+        ])
+        .toArray();
+      const revenue = result.length > 0 ? result[0].totalRevenue : 0;
+      res.send({
+        user,
+        menuItems,
+        orderItems,
+        revenue,
+      });
+    });
+    /// winner api
+
+    app.get('/winners',async(req,res)=>{
+      const result = await winnerCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.post("/winners", verifyToken, verifyAdmin, async (req, res) => {
+      const contest = req.body;
+      const result = await winnerCollection.insertOne(contest);
+      res.send(result);
+    });
+
+    // contest count api
+
+    app.get('/contestCount',async(req,res)=>{
+      const count = await contestCollection.estimatedDocumentCount()
+      res.send({count})
+    })
+
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
