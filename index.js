3abd08a9d3801780ac8d7c572ecd01b05223f7a7
saikat <tawhidulislam3482@gmail.com>
2023-11-28 14:32:21 +0600

    Optimize database queries for faster performance

--- index.js
+++ index.js
@@ -298,7 +298,7 @@ async function run() {
     })
 
 
-    // Send a ping to confirm a successful connecti
+    // Send a ping to confirm a successful connec
     // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
